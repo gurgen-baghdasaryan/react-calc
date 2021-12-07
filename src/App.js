6f174a207f69599d52bda9d4c4218c845723e089
gugohh <oohhgug@gmail.com>
2021-12-07 22:28:28 +0100

    adding the form fields

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Formik, Form } from "formik";
+import Input from "./components/Input";
 
 const Container = styled.div`
   display: flex;
@@ -15,13 +17,29 @@ const Section = styled.section`
   box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
 `
 
+const handleSubmit = () => {}
+
 function App() {
 
   return (
 
     <Container>
       <Section>
-        god bless
+        <Formik initialValues={{
+
+          deposit:'',
+          contribution:'',
+          years:'',
+          rate:'',      
+          }}
+          onSubmit={handleSubmit}>
+          <Form>
+            <Input name="deposit" label="Deposito inicial"/>
+            <Input name="contribution" label="Contributión anual"/>
+            <Input name="years" label="Años"/>
+            <Input name="rate" label="Interés estimado"/>
+          </Form>
+        </Formik>
       </Section>
     </Container>
 
